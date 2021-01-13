e1deaff629582fc0efea7f65f90e79826b5d0f98
prakashgujarati <prakash@vsc.edu.in>
2021-01-13 11:27:54 +0530

    seeder done

--- database/seeds/HarshSeeder.php
+++ database/seeds/HarshSeeder.php
@@ -12,8 +12,8 @@ class HarshSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('students')->insert(["name"=>"Harsh","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot","country"=>"india"]);
-        DB::table('students')->insert(["name"=>"Parth","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot","country"=>"india"]);
+        DB::table('students')->insert(["name"=>"Harsh","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot"]);
+        DB::table('students')->insert(["name"=>"Parth","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot"]);
         
     }
 }
--- database/seeds/StudentSeeder.php
+++ database/seeds/StudentSeeder.php
@@ -12,10 +12,10 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-       DB::table('students')->insert(["name"=>"Rutvi","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot","country"=>"india"]);
-       DB::table('students')->insert(["name"=>"Dhruv","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot","country"=>"india"]);
-       DB::table('students')->insert(["name"=>"Hinal","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot","country"=>"india"]);
-       DB::table('students')->insert(["name"=>"Rahul","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot","country"=>"india"]);
-       DB::table('students')->insert(["name"=>"Abhishek","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot","country"=>"india"]);
+       DB::table('students')->insert(["name"=>"Rutvi","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot"]);
+       DB::table('students')->insert(["name"=>"Dhruv","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot"]);
+       DB::table('students')->insert(["name"=>"Hinal","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot"]);
+       DB::table('students')->insert(["name"=>"Rahul","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot"]);
+       DB::table('students')->insert(["name"=>"Abhishek","enrollment_no"=>19090123,"roll_no"=>1,"mobile"=>"9898598985","email"=>"rutvi@gmail.com","city"=>"rajkot"]);
     }
 }
